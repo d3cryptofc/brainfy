3f32204be67d3f01ab2f00924d852f1e8c8d4174
Lelzin λ <d3cryptofc@gmail.com>
2024-10-02 18:48:13 -0300

    feat: add `parser_brackets` function to check and map all bracket positions

--- brainfy/parser.py
+++ brainfy/parser.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from .tokenizer import Tokens
 from .traceback import Traceback
 
 
@@ -18,3 +19,25 @@ def extra_end_bracket_exception(traceback: Traceback):
     )
     return RuntimeError(msg)
 
+
+def parse_brackets(tokens: list[tuple(int, str)]) -> dict[int, int]:
+    start_position_stack = []
+    all_positions_map = {}
+
+    for index, (traceback, token) in enumerate(tokens):
+        if token == Tokens.BRACKET_START:
+            start_position_stack.append((traceback, index))
+        elif token == Tokens.BRACKET_END:
+            if not start_position_stack:
+                raise extra_end_bracket_exception(traceback)
+
+            start_bracket_position = start_position_stack.pop()
+            end_bracket_position = index
+
+            all_positions_map[start_bracket_position[1]] = end_bracket_position
+            all_positions_map[end_bracket_position] = start_bracket_position[1]
+
+    if start_position_stack:
+        raise extra_start_bracket_exception(start_position_stack[0][0])
+
+    return all_positions_map
