b0dac46629ce2bdd5da61890c858567689e9ba46
Lelzin λ <d3cryptofc@gmail.com>
2024-10-02 02:59:28 -0300

    test: add `increase_must_increase_column_value`

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -12,3 +12,15 @@ from brainfy.intepreter import Interpreter
 def test_given_interpreter_length_expected_same_columns(length, columns):
     assert Interpreter(length=length).columns == columns
 
+
+@pytest.mark.parametrize(('increases', 'expected'), [
+    (1, 1),
+    (255, 255),
+    (256, 0)
+])
+def test_increase_must_increase_column_value(increases, expected):
+    i = Interpreter(length=1)
+    for _ in range(increases):
+        i.increase()
+    assert i.value == expected
+
